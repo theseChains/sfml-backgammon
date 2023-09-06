47f46847b0769c473e21e08178f0174fa05f77b9
Dusk Sherman <dusksher@ge-a3.kzn.21-school.ru>
2023-09-06 17:37:13 +0300

    Add constants to the application class

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,7 +1,9 @@
 #include "Application.hpp"
 
+#include "Constants.hpp"
+
 Application::Application()
-    : m_window{ sf::VideoMode{ 500, 500 }, "backgammon" }
+    : m_window{ sf::VideoMode{ constants::windowWidth, constants::windowHeight }, "backgammon" }
 {}
 
 void Application::run()
@@ -29,5 +31,14 @@ void Application::processInput()
 void Application::draw()
 {
     m_window.clear();
+
+    for (int i{ 0 }; i < 15; ++i)
+    {
+        sf::CircleShape circle{ constants::chipRadius };
+        circle.setPosition(0.0f, 50.0f * static_cast<float>(i));
+        circle.setFillColor(sf::Color::Cyan);
+        m_window.draw(circle);
+    }
+
     m_window.display();
 }
