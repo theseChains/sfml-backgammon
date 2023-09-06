ae642cc0fdacfee1cf406189323dae727d0e3a84
Dusk Sherman <dusksher@ge-a3.kzn.21-school.ru>
2023-09-06 17:36:02 +0300

    Add constants

---
+++ src/Constants.hpp
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace constants
+{
+    inline constexpr int windowWidth{ 600 };
+    inline constexpr int windowHeight{ 1000 };
+    inline constexpr float chipRadius{ 25.0f };
+};
+
+#endif
