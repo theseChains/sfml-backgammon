b9d3f860d8bae37a32824fdec903344156f38d6e
Dusk Sherman <dusksher@ge-a3.kzn.21-school.ru>
2023-09-09 19:01:37 +0300

    Add aliases

---
+++ src/Aliases.hpp
@@ -0,0 +1,11 @@
+#ifndef ALIASES_HPP
+#define ALIASES_HPP
+
+#include <array>
+
+#include "Chip.hpp"
+#include "Constants.hpp"
+
+using PlayerChips = std::array<Chip, constants::numberOfChips>;
+
+#endif
