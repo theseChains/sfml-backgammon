35fb0b1bd05c3081d90e5796211c0a944fac0702
Dusk Sherman <dusksher@ge-a3.kzn.21-school.ru>
2023-09-09 19:03:15 +0300

    Add the chip class

---
+++ src/Chip.cpp
@@ -0,0 +1,32 @@
+#include "Chip.hpp"
+
+#include "Constants.hpp"
+
+Textures::ID toTextureID(ChipColor color)
+{
+    switch (color)
+    {
+        case ChipColor::white:
+            return Textures::ID::whiteChip;
+        case ChipColor::black:
+            return Textures::ID::blackChip;
+        default:
+            return Textures::ID{};
+    }
+}
+
+Chip::Chip(const sf::Vector2f& position, ChipColor color, const TextureHolder& textures)
+    : m_position{ position },
+      m_color{ color },
+      m_chipCircle{},
+      m_texture{ textures.get(toTextureID(color)) }
+{
+    m_chipCircle.setPosition(position);
+    m_chipCircle.setRadius(constants::chipRadius);
+    m_chipCircle.setTexture(&m_texture);
+}
+
+void Chip::draw(sf::RenderWindow& window)
+{
+    window.draw(m_chipCircle);
+}
--- src/Chip.hpp
+++ src/Chip.hpp
@@ -1,9 +1,25 @@
 #ifndef CHIP_HPP
 #define CHIP_HPP
 
+#include <SFML/Graphics.hpp>
+
+#include "ChipColor.hpp"
+#include "ResourceIdentifiers.hpp"
+#include "ResourceHolder.hpp"
+
 class Chip
 {
+public:
     Chip() = default;
+    Chip(const sf::Vector2f& position, ChipColor color, const TextureHolder& textures);
+
+    void draw(sf::RenderWindow& window);
+
+private:
+    sf::Vector2f m_position;
+    ChipColor m_color;
+    sf::CircleShape m_chipCircle;
+    sf::Texture m_texture;
 };
 
 #endif
