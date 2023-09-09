e54ea03be8bc2c2233b04e8a66f838af817d3e8a
Dusk Sherman <dusksher@ge-a3.kzn.21-school.ru>
2023-09-09 19:02:58 +0300

    Add chip diameter constant

--- src/Constants.hpp
+++ src/Constants.hpp
@@ -5,7 +5,10 @@ namespace constants
 {
     inline constexpr int windowWidth{ 600 };
     inline constexpr int windowHeight{ 1000 };
+    inline constexpr int numberOfChips{ 15 };
+
     inline constexpr float chipRadius{ 25.0f };
+    inline constexpr float chipDiameter{ chipRadius * 2.0f };
 };
 
 #endif
